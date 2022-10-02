88e2a189883cf7ebf3f319b68b2823e406317246
Mauri Claudio <mauri.claudio@gmail.com>
2022-10-02 11:06:07 +0200

    Modificato sorgente

--- demo/src/main/java/it/limontainformatica/course/App.java
+++ demo/src/main/java/it/limontainformatica/course/App.java
@@ -10,6 +10,8 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
+        
         System.out.println("Hello world.");
+        
     }
 }
