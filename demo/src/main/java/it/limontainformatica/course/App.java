45f9cd0103db4132f5276f0dd461bc65091e8beb
Mauri Claudio <mauri.claudio@gmail.com>
2022-10-02 10:59:22 +0200

    OK

---
+++ demo/src/main/java/it/limontainformatica/course/App.java
@@ -0,0 +1,15 @@
+package it.limontainformatica.course;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
