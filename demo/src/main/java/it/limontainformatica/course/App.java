6f3cbd72833b9cd9ca75cb89695e06133656f770
Mauri Claudio <mauri.claudio@gmail.com>
2022-10-02 11:03:37 +0200

    OK2

--- demo/src/main/java/it/limontainformatica/course/App.java
+++ demo/src/main/java/it/limontainformatica/course/App.java
@@ -10,6 +10,6 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello world.");
     }
 }
